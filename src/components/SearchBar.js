e29e1d4b8f1a0f6ba3b365e6123c4e438ba62f0a
AnkushSharma2698 <ankush1@ualberta.ca>
2018-06-25 13:31:08 -0600

    search bar added

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,10 +1,16 @@
 import React,{Component} from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import MenuItem from '@material-ui/core/MenuItem';
+import TextField from '@material-ui/core/TextField';
 
 
 class SearchBar extends Component {
   render() {
     return (
-      <div>Text</div>
+      <form>
+        <TextField/>
+      </form>
     );
   };
 }
