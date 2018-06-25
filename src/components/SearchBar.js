1cce249510321eda1cbc24be10e65fd91350f7b3
AnkushSharma2698 <ankush1@ualberta.ca>
2018-06-25 14:14:18 -0600

    search bar centered

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,14 +3,22 @@ import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import MenuItem from '@material-ui/core/MenuItem';
 import TextField from '@material-ui/core/TextField';
+import '../Styling/SearchBar.css';
 
 
 class SearchBar extends Component {
   render() {
     return (
-      <form>
-        <TextField/>
-      </form>
+      <div className = "parent">
+        <form className = 'SearchBar'>
+          <TextField
+            id='Search'
+            label = "Find Artist"
+            className = "Search"
+            margin= "normal"
+          />
+        </form>
+      </div>
     );
   };
 }
