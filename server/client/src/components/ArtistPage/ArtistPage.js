37d64b5413bc097ca37b9bf813b707983abf9643
Roy Chowdhury <chowdhury.roy4@gmail.com>
2018-07-14 14:47:07 -0600

    finished artist image

--- server/client/src/components/ArtistPage/ArtistPage.js
+++ server/client/src/components/ArtistPage/ArtistPage.js
@@ -1,57 +1,91 @@
-import React, {Component} from 'react';
-import './css/ArtistPage.css'
-import axios from 'axios';
+import React, { Component } from "react";
+import "./css/ArtistPage.css";
+import axios from "axios";
+import { spotifySearchURL } from "../../SpotifyConstants/SpotifyConstants";
+import Particles from "react-particles-js";
+import particlesOptions from "../../Background/particlesOptions";
 
 // ===== Implement an artist card with its respective css component === //
 
 class ArtistPage extends Component {
   constructor(props) {
     super(props);
-    console.log('In ArtistPage: ',this.props.inputState)
+    this.state = {
+      imageLink: ""
+    };
+    console.log("In ArtistPage: ", this.props.inputState);
   }
 
   componentWillMount() {
     //get access tokens
-    let accessToken= this.getHashParams().access_token;
-    let artistName= this.getHashParams().artist_name;
-
+    let accessToken = this.getHashParams().access_token;
+    let artistName = this.getHashParams().artist_name;
+    axios
+      .get(
+        `${spotifySearchURL}${artistName}&type=artist&access_token=${accessToken}`
+      )
+      .then(response => {
+        console.log(response);
+        console.log(response.data.artists.items[0].images[0].url);
+        this.setState({
+          imageLink: response.data.artists.items[0].images[0].url
+        });
+        //console.log(response.data.artists);
+        //console.log(response.data.artists.items.images[0].url);
+        //this.setState({imageLink: response.})
+        // alert(`You searched for ${response.data.artists.items[0].name} and the
+        // artist ID is ${response.data.artists.items[0].id}`);
+        // //Sending the Artist Id back to the App Component;
+        // this.props.getArtistId(response.data.artists.items[0].id);
+      });
 
     //get artist name
   }
 
-//Used to Parse the parameters
+  //Used to Parse the parameters
   getHashParams() {
     var hashParams = {};
-    var e, r = /([^&;=]+)=?([^&;]*)/g,
-        q = window.location.hash.substring(1);
-    e = r.exec(q)
+    var e,
+      r = /([^&;=]+)=?([^&;]*)/g,
+      q = window.location.hash.substring(1);
+    e = r.exec(q);
     while (e) {
-       hashParams[e[1]] = decodeURIComponent(e[2]);
-       e = r.exec(q);
+      hashParams[e[1]] = decodeURIComponent(e[2]);
+      e = r.exec(q);
     }
     return hashParams;
   }
 
-
-
   render() {
     return (
       <div className="blog-card spring-fever">
-          <img src="https://i.scdn.co/image/eb266625dab075341e8c4378a177a27370f91903" alt=""/>
+        <Particles
+          className="particles"
+          id="particlesOptions"
+          params={particlesOptions}
+        />
+        <img src={this.state.imageLink} alt="" />
         <div className="title-content">
           <h3>SPRING FEVER</h3>
           <hr />
-          <div className="intro">Yllamco laboris nisi ut aliquip ex ea commodo.</div>
-        </div>{/* /.title-content */}
+          <div className="intro">
+            Yllamco laboris nisi ut aliquip ex ea commodo.
+          </div>
+        </div>
+        {/* /.title-content */}
         <div className="card-info">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim.
-        </div>{/* /.card-info */}
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim.
+        </div>
+        {/* /.card-info */}
         <div className="utility-info">
           <ul className="utility-list">
             <li className="comments">12</li>
             <li className="date">03.12.2015</li>
           </ul>
-        </div>{/* /.utility-info */}
+        </div>
+        {/* /.utility-info */}
         {/* overlays */}
         <div className="gradient-overlay" />
         <div className="color-overlay" />
--- server/client/src/components/Dashboard/Dashboard.js
+++ server/client/src/components/Dashboard/Dashboard.js
@@ -1,29 +1,28 @@
-import React, {Component} from 'react';
-import './Dashboard.css';
+import React, { Component } from "react";
+import "./Dashboard.css";
 //Import particles
 //Importing particles
-import Particles from 'react-particles-js';
-import particlesOptions from '../../Background/particlesOptions';
+import Particles from "react-particles-js";
+import particlesOptions from "../../Background/particlesOptions";
 
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 //Import Axios
-import axios from 'axios';
+import axios from "axios";
 
-import NavBar from '../../NavBar/NavBar';
+import NavBar from "../../NavBar/NavBar";
 
 //Import Spotify url constants
-import {spotifySearchURL} from '../../SpotifyConstants/SpotifyConstants';
+import { spotifySearchURL } from "../../SpotifyConstants/SpotifyConstants";
 
 //import spotify requirements
-import SpotifyWebApi from 'spotify-web-api-js';
+import SpotifyWebApi from "spotify-web-api-js";
 const spotifyApi = new SpotifyWebApi();
 
 //Social Media styling
-const mediaStyle  = {
-  fill:'currentcolor'
-}
-
+const mediaStyle = {
+  fill: "currentcolor"
+};
 
 class Dashboard extends Component {
   constructor(props) {
@@ -34,100 +33,118 @@ class Dashboard extends Component {
       spotifyApi.setAccessToken(token);
     }
     this.state = {
-      inputState:'',
-      artistId:'',
-      access__Token:'',
-      topArtists:[]
-    }
+      inputState: "",
+      artistId: "",
+      access__Token: "",
+      topArtists: []
+    };
   }
 
   //Get the users top artists
   componentWillMount() {
-    spotifyApi.getMyTopArtists()
-      .then((response) => {
-        console.log(response)
-        response.items.map(item => this.setState({topArtists:this.state.topArtists.concat([item.name+','])}))
-        //
-      })
-
+    spotifyApi.getMyTopArtists().then(response => {
+      console.log(response);
+      response.items.map(item =>
+        this.setState({
+          topArtists: this.state.topArtists.concat([item.name + ", "])
+        })
+      );
+      //
+    });
   }
 
   getHashParams() {
     var hashParams = {};
-    var e, r = /([^&;=]+)=?([^&;]*)/g,
-        q = window.location.hash.substring(1);
-    e = r.exec(q)
+    var e,
+      r = /([^&;=]+)=?([^&;]*)/g,
+      q = window.location.hash.substring(1);
+    e = r.exec(q);
     while (e) {
-       hashParams[e[1]] = decodeURIComponent(e[2]);
-       e = r.exec(q);
+      hashParams[e[1]] = decodeURIComponent(e[2]);
+      e = r.exec(q);
     }
     return hashParams;
   }
 
-
-  searchSportifyArtists = (event,getHashParams) => {
-    let parsed=this.getHashParams();
+  searchSportifyArtists = (event, getHashParams) => {
+    let parsed = this.getHashParams();
     let accessToken = parsed.access_token;
     //give the state access to the accessToken
 
-    axios.get(`${spotifySearchURL}${this.state.inputState}&type=artist&access_token=${accessToken}`)
-      .then((response)=> {
+    axios
+      .get(
+        `${spotifySearchURL}${
+          this.state.inputState
+        }&type=artist&access_token=${accessToken}`
+      )
+      .then(response => {
         console.log(response.data.artists.items[0]);
-        alert(`You searched for ${response.data.artists.items[0].name} and the
-          artist ID is ${response.data.artists.items[0].id}`)
+
         //Sending the Artist Id back to the App Component;
         this.props.getArtistId(response.data.artists.items[0].id);
       })
       .catch(error => {
-        alert('Do not leave the search bar empty');
-      })
-  }
+        alert("Do not leave the search bar empty");
+      });
+  };
 
-//This Function keeps track of changes to the input
-  onInputChange=(event,token) => {
-    this.setState({inputState:event.target.value});
-  }
+  //This Function keeps track of changes to the input
+  onInputChange = (event, token) => {
+    this.setState({ inputState: event.target.value });
+  };
 
-  getTheID = (getHashParams)=> {
-    let parsed=this.getHashParams();
+  getTheID = getHashParams => {
+    let parsed = this.getHashParams();
     let accessToken = parsed.access_token;
     //give the state access to the accessToken
 
-    axios.get(`${spotifySearchURL}${this.state.inputState}&type=artist&access_token=${accessToken}`)
-      .then((response)=> {
-        return(
-          this.props.getArtistId(response.data.artists.items[0].id)
-        )
-      })
-  }
-
-
-
+    axios
+      .get(
+        `${spotifySearchURL}${
+          this.state.inputState
+        }&type=artist&access_token=${accessToken}`
+      )
+      .then(response => {
+        return this.props.getArtistId(response.data.artists.items[0].id);
+      });
+  };
 
   render() {
-    return(
-      <div className='dashboard flex'>
-        <Particles className='particles' id='particlesOptions'
-             params={particlesOptions}
-           />
+    return (
+      <div className="dashboard flex">
+        <Particles
+          className="particles"
+          id="particlesOptions"
+          params={particlesOptions}
+        />
         {/* ====Navbar goes here ===== */}
-        <NavBar/>
+        <NavBar />
         <div>
-            <h1>Find Your Favorite Artists!</h1>
+          <h1>Find Your Favorite Artists!</h1>
         </div>
 
-        <div id='search-box' className='search-box'>
-          <input onChange={this.onInputChange} type='text' className='typeahead' placeholder='Search for any artist'/>
+        <div id="search-box" className="search-box">
+          <input
+            onChange={this.onInputChange}
+            type="text"
+            className="typeahead"
+            placeholder="Search for any artist"
+          />
         </div>
         {/* //==========Submit Button========// */}
-        <div className='ph3 buttondiv'>
+        <div className="ph3 buttondiv">
           <Link
-            to =
-            {{pathname:'/ArtistPage',
-              hash:`access_token=${this.getHashParams().access_token}&artist_name=${this.state.inputState}`
+            to={{
+              pathname: "/ArtistPage",
+              hash: `access_token=${
+                this.getHashParams().access_token
+              }&artist_name=${this.state.inputState}`
             }}
-            className='f6 grow link dim ph3 pv2 mb2 dib white w-100 submit'
-            onClick={()=>this.searchSportifyArtists()}>Submit</Link>
+            className="f6 grow link dim ph3 pv2 mb2 dib white w-100 submit"
+            onClick={() => this.searchSportifyArtists()}
+          >
+            Submit
+          </Link>
         </div>
 
         {/* /=========TOP ARTISTS CARD=========/ */}
@@ -140,32 +157,41 @@ class Dashboard extends Component {
           </div>
         </article>
 
-
         {/* =======ADDING SOCIALS ON THE BOTTOM OF THE PAGE=========== */}
         <footer className="pv4 ph3 ph5-ns tc">
-          <a className="link dim gray dib h2 w2 br-100 mr3 pa2 bg-near-white ba b--black-10" href="#as" title="">
+          <a
+            className="link dim gray dib h2 w2 br-100 mr3 pa2 bg-near-white ba b--black-10"
+            href="#as"
+            title=""
+          >
             <svg data-icon="facebook" viewBox="0 0 32 32" style={mediaStyle}>
               <title>facebook icon</title>
-              <path d="M8 12 L13 12 L13 8 C13 2 17 1 24 2 L24 7 C20 7 19 7 19 10 L19 12 L24 12 L23 18 L19 18 L19 30 L13 30 L13 18 L8 18 z"></path>
+              <path d="M8 12 L13 12 L13 8 C13 2 17 1 24 2 L24 7 C20 7 19 7 19 10 L19 12 L24 12 L23 18 L19 18 L19 30 L13 30 L13 18 L8 18 z" />
             </svg>
           </a>
-          <a className="link dim gray dib h2 w2 br-100 mr3 pa2 bg-near-white ba b--black-10" href="#asd" title="">
+          <a
+            className="link dim gray dib h2 w2 br-100 mr3 pa2 bg-near-white ba b--black-10"
+            href="#asd"
+            title=""
+          >
             <svg data-icon="twitter" viewBox="0 0 32 32" style={mediaStyle}>
               <title>twitter icon</title>
-              <path d="M2 4 C6 8 10 12 15 11 A6 6 0 0 1 22 4 A6 6 0 0 1 26 6 A8 8 0 0 0 31 4 A8 8 0 0 1 28 8 A8 8 0 0 0 32 7 A8 8 0 0 1 28 11 A18 18 0 0 1 10 30 A18 18 0 0 1 0 27 A12 12 0 0 0 8 24 A8 8 0 0 1 3 20 A8 8 0 0 0 6 19.5 A8 8 0 0 1 0 12 A8 8 0 0 0 3 13 A8 8 0 0 1 2 4"></path>
+              <path d="M2 4 C6 8 10 12 15 11 A6 6 0 0 1 22 4 A6 6 0 0 1 26 6 A8 8 0 0 0 31 4 A8 8 0 0 1 28 8 A8 8 0 0 0 32 7 A8 8 0 0 1 28 11 A18 18 0 0 1 10 30 A18 18 0 0 1 0 27 A12 12 0 0 0 8 24 A8 8 0 0 1 3 20 A8 8 0 0 0 6 19.5 A8 8 0 0 1 0 12 A8 8 0 0 0 3 13 A8 8 0 0 1 2 4" />
             </svg>
           </a>
-          <a className="link dim gray dib br-100 h2 w2 mr3 pa2 bg-near-white ba b--black-10" href="https://github.com/AnkushSharma2698/MusicalMap" title="">
+          <a
+            className="link dim gray dib br-100 h2 w2 mr3 pa2 bg-near-white ba b--black-10"
+            href="https://github.com/AnkushSharma2698/MusicalMap"
+            title=""
+          >
             <svg data-icon="github" viewBox="0 0 32 32" style={mediaStyle}>
               <title>github icon</title>
-              <path d="M0 18 C0 12 3 10 3 9 C2.5 7 2.5 4 3 3 C6 3 9 5 10 6 C12 5 14 5 16 5 C18 5 20 5 22 6 C23 5 26 3 29 3 C29.5 4 29.5 7 29 9 C29 10 32 12 32 18 C32 25 30 30 16 30 C2 30 0 25 0 18 M3 20 C3 24 4 28 16 28 C28 28 29 24 29 20 C29 16 28 14 16 14 C4 14 3 16 3 20 M8 21 A1.5 2.5 0 0 0 13 21 A1.5 2.5 0 0 0 8 21 M24 21 A1.5 2.5 0 0 0 19 21 A1.5 2.5 0 0 0 24 21 z"></path>
+              <path d="M0 18 C0 12 3 10 3 9 C2.5 7 2.5 4 3 3 C6 3 9 5 10 6 C12 5 14 5 16 5 C18 5 20 5 22 6 C23 5 26 3 29 3 C29.5 4 29.5 7 29 9 C29 10 32 12 32 18 C32 25 30 30 16 30 C2 30 0 25 0 18 M3 20 C3 24 4 28 16 28 C28 28 29 24 29 20 C29 16 28 14 16 14 C4 14 3 16 3 20 M8 21 A1.5 2.5 0 0 0 13 21 A1.5 2.5 0 0 0 8 21 M24 21 A1.5 2.5 0 0 0 19 21 A1.5 2.5 0 0 0 24 21 z" />
             </svg>
           </a>
         </footer>
-
       </div>
-
-    )
+    );
   }
 }
 
